ff14909e3e169b2dff668a5b6270a803fdde05c3
Aviral <tarun.a@husky.neu.edu>
2018-10-20 00:27:45 -0400

    babelrc ,gitignore added

--- Client/screens/HomeScreen.js
+++ Client/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
   Image,
+  Button,
   Platform,
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
@@ -18,50 +20,66 @@ export default class HomeScreen extends React.Component {
   };
 
   render() {
+
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+      <ScrollView style={{padding: 20}}>
+          <Text 
+              style={{fontSize: 27}}>
+              Login
+          </Text>
+          <TextInput placeholder='Username' />
+          <TextInput placeholder='Password' />
+          <View style={{margin:7}} />
+          <Button 
+                    // onPress={this.props.onLoginPress}
+                    title="Submit"
+                />
+            </ScrollView>
+      )
+    // return (
+    //   <View style={styles.container}>
+    //     <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+    //       <View style={styles.welcomeContainer}>
+    //         <Image
+    //           source={
+    //             __DEV__
+    //               ? require('../assets/images/robot-dev.png')
+    //               : require('../assets/images/robot-prod.png')
+    //           }
+    //           style={styles.welcomeImage}
+    //         />
+    //       </View>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+    //       <View style={styles.getStartedContainer}>
+    //         {this._maybeRenderDevelopmentModeWarning()}
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+    //         <Text style={styles.getStartedText}>Get started by opening</Text>
 
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
+    //         <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+    //           <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+    //         </View>
 
-            <Text style={styles.getStartedText}>
-              Hello world
-            </Text>
-          </View>
+    //         <Text style={styles.getStartedText}>
+    //           Hello world
+    //         </Text>
+    //       </View>
 
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
+    //       <View style={styles.helpContainer}>
+    //         <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
+    //           <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+    //         </TouchableOpacity>
+    //       </View>
+    //     </ScrollView>
 
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+    //     <View style={styles.tabBarInfoContainer}>
+    //       <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
 
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
+    //       <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+    //         <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
+    //       </View>
+    //     </View>
+    //   </View>
+    // );
   }
 
   _maybeRenderDevelopmentModeWarning() {
